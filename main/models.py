6069267ab30661cde14ab40c39f7f5c233867afb
lucidd27212 <gilangadjiesaputra@gmail.com>
2025-09-15 15:16:02 +0700

    tugas 3

--- main/models.py
+++ main/models.py
@@ -1,24 +1,9 @@
-from django.db import models # type: ignore
+from django.db import models
 
 class Product(models.Model):
-    CATEGORY_CHOICES = [
-        ('jersey', 'Jersey'),
-        ('shoes', 'Shoes'),
-        ('ball', 'Ball'),
-        ('accessory', 'Accessory'),
-        ('other', 'Other'),
-    ]
-
     name = models.CharField(max_length=255)
-    price = models.IntegerField()
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    stock = models.IntegerField()
     description = models.TextField()
-    thumbnail = models.URLField(blank=True, null=True)
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='other')
-    is_featured = models.BooleanField(default=False)
-
-    # Tambahan opsional
-    stock = models.PositiveIntegerField(default=0)
-    brand = models.CharField(max_length=100, blank=True, null=True)
-
-    def __str__(self):
-        return self.name
+    image_url = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
